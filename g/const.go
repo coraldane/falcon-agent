96ad3a92d11e92c3d8c1c6c69cf745ff7a14d88a
coraldane <coraldane@163.com>
2021-07-20 18:05:31 +0800

    update version

--- g/const.go
+++ g/const.go
@@ -10,8 +10,9 @@ import (
 // 5.0.0: 支持通过配置控制是否开启/run接口；收集udp流量数据；du某个目录的大小
 // 5.1.0: 同步插件的时候不再使用checksum机制
 // 5.1.1: 修复往多个transfer发送数据的时候crash的问题
+// 5.1.4: 20210720 修改依赖的common地址为gitlab.tarzip.com/open-falcon/common
 const (
-	VERSION          = "5.1.3"
+	VERSION          = "5.1.4"
 	COLLECT_INTERVAL = time.Second
 	URL_CHECK_HEALTH = "url.check.health"
 	NET_PORT_LISTEN  = "net.port.listen"
